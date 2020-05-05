4d10a677897eedcd12378277eb126b4e54582368
Blurgyy <blurgyy@gmail.com>
2020-05-05 18:46:32 +0800

    Created window using GLFW
    Signed-off-by: Blurgyy <blurgyy@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <vulkan/vulkan.h>
 #include <iostream>
 #include <exception>
 #include <cstdlib>
 
+uint32_t window_width = 1024;
+uint32_t window_height = 768;
+
 class TriangleApplication
 {
 private:
+    GLFWwindow* window;
+
+    void initWindow(){
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(window_width, window_height, "vulkanTest", nullptr, nullptr);
+    }
     void initVulkan(){}
-    void mainLoop(){}
-    void cleanup(){}
+    void mainLoop(){
+        while (!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+        }
+    }
+    void cleanup(){
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
 public:
     void run(){
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
