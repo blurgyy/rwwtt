60de71fee2d69e523176dfecfb02f308135cdf5c
Blurgyy <blurgyy@gmail.com>
2020-05-10 02:32:32 +0800

    Changed default resolution to 512x512, to render faster
    Signed-off-by: Blurgyy <blurgyy@gmail.com>

--- src/global.hpp
+++ src/global.hpp
@@ -17,8 +17,8 @@
 
 const int max_frames_in_flight = 2;
 
-const uint32_t window_width = 1024;
-const uint32_t window_height = 768;
+const uint32_t window_width = 512;
+const uint32_t window_height = 512;
 
 const std::vector<const char*> validationLayers{
     "VK_LAYER_KHRONOS_validation"
